70719232b689747041cca88293a70812e84c880f
kieranwatt <kieranmwatt@gmail.com>
2023-10-05 09:24:56 -0400

    thursdayv1

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 let uweight = Number(prompt("enter number"));
 let uheight = Number(prompt("enter number"));
 function BMIcalc(uheight, uweight); {
-    return(uweight / (uheight*uheight));
+    const BMI = ((uweight / (uheight)(uheight))*10000);
+    return BMI;
 }
+const uweight = String(prompt("Please enter weight"));
+const uheight = String(prompt("Please enter height"));
+const BMI = BMIcalc(uheight, uweight);
